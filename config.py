7973cf05624013fd097375286fd203c1909e8569
Leaky42 <agateaugg+1@protonmail.com>
2022-03-15 14:33:00 +0100

    Yet another leak

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ db_user = "john"
 db_password = r"f1jer3iojnionvbfAZ"
 
 byte_api_key = b"frefegj45435wfreg"
-f_string_password = f"we'dBetterIgnore{os.environ['DB_PASSWORD']}That"
+f_string_password = f"we'dBetterIgnore{os.environ['DB_PASSWORD']}That0rNot"
