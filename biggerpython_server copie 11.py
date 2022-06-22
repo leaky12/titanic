b4da0a63919d90fef694455cf63237e8ee6fb8c6
Serial Leaker <agateaugg@protonmail.com>
2022-06-22 14:46:09 +0200

    Add long file name, with spaces

---
+++ biggerpython_server copie 11.py
@@ -0,0 +1,9 @@
+import os
+
+db_user = "john"
+db_password = r"f1jer3iojnionvbfAZ"
+
+byte_api_key = \
+    b"frefegj45435wfreg"
+
+f_string_password = f"we'dBetterIgnore{os.environ['DB_PASSWORD']}That0rNot"
